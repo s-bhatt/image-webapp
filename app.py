7d888e5f49a11a8fcfb2c3cb55307276e7174feb
Shivang Bhatt <shivang@Shivangs-MBP.fios-router.home>
2017-12-10 20:11:23 -0500

    added website buttons

--- app.py
+++ app.py
@@ -1,67 +1,73 @@
 import os
 
 from flask import Flask, request, render_template, send_from_directory
-import mechanize
+# import mechanize
 import re
 
 import time
 # from lxml import html  
 # import xlwt 
 # import xlrd 
-from django.utils.http import urlquote 
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
+# from django.utils.http import urlquote 
+# from selenium import webdriver
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.webdriver.common.keys import Keys
 __author__ = 'ibininja'
 
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-driver = webdriver.Chrome('C:\Users\Dev\Downloads\chromedriver_win32\chromedriver.exe') 
-#change location here, download chrome driver
-driver.maximize_window()
-def search_amazon(search_string):
-    static_search_amazon = 'https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords='
-    driver.get(static_search_amazon+urlquote(search_string).encode('utf8'))
+# driver = webdriver.Chrome('C:\Users\Dev\Downloads\chromedriver_win32\chromedriver.exe') 
+# #change location here, download chrome driver
+# driver.maximize_window()
+line=""
 
-def search_walmart(search_string):
+@app.route("/search_amazon", methods=["POST"])
+def search_amazon(search_string=line):
+    static_search_amazon = 'https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords='
+#     driver.get(static_search_amazon+urlquote(search_string).encode('utf8'))
+    return render_template("amazon.html")
+
+@app.route("/search_walmart", methods=["POST"])
+def search_walmart(search_string=line):
     static_search_walmart1 = 'https://www.walmart.com/search/?query='
-    static_search_walmart2 = '&cat_id=0'
-    driver.get(static_search_walmart1+urlquote(search_string).encode('utf8')+static_search_walmart2)
+#     static_search_walmart2 = '&cat_id=0'
+#     driver.get(static_search_walmart1+urlquote(search_string).encode('utf8')+static_search_walmart2)
 
 @app.route("/")
 def index():
     return render_template("upload.html")
 
-def object_recognizer():
-    f = open("temp.txt","w+")
-    f.write("Line 1 \n")
-    f.write("Line 2 \n")
-    f.close();
-    delete_line()
-def delete_line():
-    f = open("temp.txt","r+")
-    lines = f.readlines()
-    f.seek(0)
-    #get the line you want no need to delete line
-    # for line in lines:
-    #     if some condition:
-    #         get line
-    #search websites for the product
-    search_amazon(line)
-    search_walmart(line)
+# def object_recognizer():
+#     f = open("temp.txt","w+")
+#     f.write("Line 1 \n")
+#     f.write("Line 2 \n")
+#     f.close();
+#     delete_line()
+# def delete_line():
+#     f = open("temp.txt","r+")
+#     lines = f.readlines()
+#     f.seek(0)
+#     #get the line you want no need to delete line
+#     # for line in lines:
+#     #     if some condition:
+#     #         get line
+#     #search websites for the product
+#     search_amazon(line)
+#     search_walmart(line)
 
 
-@app.route("/complete", methods=["POST"])
-def detect_function():
-    #call object recognzer here
-    file = object_recognizer()
-    print "doneee"
+# @app.route("/complete", methods=["POST"])
+# def detect_function():
+#     #call object recognzer here
+#     file = object_recognizer()
+#     print "doneee"
 
 @app.route("/foobar", methods=["POST"])
 def foobar():
     print "inside foobar"
+    return render_template("foobar.html")
 
 @app.route("/upload", methods=["POST"])
 def upload():
